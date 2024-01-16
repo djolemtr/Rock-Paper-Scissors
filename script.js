459776b310b7e893b3768eaa5e070c8d1448e648
djolemtr <113414071+djolemtr@users.noreply.github.com>
2024-01-16 14:04:01 +0100

    Add HTML UI and update logic

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+let playerWins = 0;
+let computerWins = 0;
+
+let roundNumber = 1;
+const round = document.getElementById('round');
+round.innerHTML = roundNumber;
+
+function game(playerSelection) {
+
+    const computerSelection = getRandomChoice();
+    const resultDiv = document.getElementById('result');
+
+
+    resultDiv.innerHTML = playRound(playerSelection, computerSelection);
+
+    // Update the score display
+    updateScore();
+
+
+
+    if (roundNumber === 5) {
+        endGame();
+    }
+}
+
+function updateRound() {
+    roundNumber++;
+}
+
+function updateScore() {
+    const scoreDiv = document.getElementById('score');
+    scoreDiv.innerHTML = `Player: ${playerWins} | Computer: ${computerWins}`;
+}
+
+function endGame() {
+
+    resultDiv.innerHTML = "";
+    const resultDiv = document.getElementById('result');
+    if (playerWins > computerWins) {
+        resultDiv.innerHTML += "<br>You won the game!";
+    } else if (computerWins > playerWins) {
+        resultDiv.innerHTML += "<br>You lost the game.";
+    } else {
+        resultDiv.innerHTML += "<br>It's a tie.";
+    }
+
+    // Optionally, reset the game by resetting playerWins and computerWins
+    playerWins = 0;
+    computerWins = 0;
+}
+
+
+
 function getRandomChoice() {
 
     var number = Math.floor(Math.random() * 3) + 1;
@@ -15,35 +68,37 @@ function getRandomChoice() {
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == "rock" && computerSelection == "scissors") {
+        playerWins++;
         return "You won!"
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You lost!"
+        computerWins++;
+        return "You lost."
     } else if (playerSelection == "rock" && computerSelection == "rock") {
-        return "It's a tie!"
+        return "It's a tie."
     }
 
     if (playerSelection == "paper" && computerSelection == "rock") {
+        playerWins++;
         return "You won!"
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You lost!"
-    } else if (playerSelection == "paper" && computerSelection == "paper") {
-        return "It's a tie!"
+        computerWins++;
+        return "You lost."
+    } else if (playerSelection == "gamepaper" && computerSelection == "paper") {
+        return "It's a tie."
     }
 
     if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerWins++;
         return "You won!"
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "You lost!"
+        computerWins++;
+        return "You lost."
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return "It's a tie!"
+        return "It's a tie."
     }
 
 
 
 }
 
-const playerSelection = "scissors";
-const computerSelection = getRandomChoice();
 
-console.log(playerSelection + " " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
